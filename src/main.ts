04bd298ac4f7f2ea41990157e9109bcc70bc2146
pwhb <pwhbdev@gmail.com>
2023-12-09 19:49:15 +0630

    configured port

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap()
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
